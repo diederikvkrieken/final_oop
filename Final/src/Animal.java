459c736142c5c0c882a58df9e3e39bb5d1f21a0b
Diederik van Krieken <diederikvkrieken@gmail.com>
2013-06-10 13:24:45 +0200

    First implementation of animal and prey, predator

---
+++ Final/src/Animal.java
@@ -0,0 +1,51 @@
+
+public class Animal {
+	private int lifeEnergyPool;
+	private int turnCost;
+	private int x, y;
+	
+	public void look(){
+		for(x = (getX()-1);x<(getX()+1);x++){
+			for(y = (getY()-1);y<(getY()+1);y++){
+				
+			}
+		}
+	}
+	
+	public void move(){
+		
+	}
+
+	public int getLifeEnergyPool() {
+		return lifeEnergyPool;
+	}
+
+	public void setLifeEnergyPool(int lifeEnergyPool) {
+		this.lifeEnergyPool = lifeEnergyPool;
+	}
+
+	public int getTurnCost() {
+		return turnCost;
+	}
+
+	public void setTurnCost(int turnCost) {
+		this.turnCost = turnCost;
+	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+}
